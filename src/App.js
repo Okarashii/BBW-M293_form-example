f2b482175689598268feae3f4561dc1f9890e806
Fujitaka Okarashii <nuttli.christian@gmail.com>
2024-03-17 10:08:22 +0100

    - solved task 1.2

--- src/App.js
+++ src/App.js
@@ -18,18 +18,20 @@ function Flash() {
     const [flash, setFlash] = useState(false);
 
     return (
-        <div className="flash-wrapper">
-            <input type="radio" id="flash-on" name="flash" checked={flash} onChange={() => setFlash((f) => !f)}/>
-            <label htmlFor="flash-on">
-                Blitz ein
-            </label>
-            <input type="radio" id="flash-off" name="flash" checked={!flash} onChange={() => setFlash((f) => !f)}/>
-            <label htmlFor="flash-off">
-                Blitz aus
-            </label>
+        <>
+            <div className="flash-wrapper">
+                <input type="radio" id="flash-on" name="flash" checked={flash} onChange={() => setFlash((f) => !f)}/>
+                <label htmlFor="flash-on">
+                    Blitz ein
+                </label>
+                <input type="radio" id="flash-off" name="flash" checked={!flash} onChange={() => setFlash((f) => !f)}/>
+                <label htmlFor="flash-off">
+                    Blitz aus
+                </label>
 
+            </div>
             <DebugButton log={[["Flash", flash]]}/>
-        </div>
+        </>
     );
 }
 
@@ -39,15 +41,39 @@ function CheckBoxes() {
     const [extraOnions, setExtraOnions] = useState(false);
 
     return (
-        <div className="checkboxes-wrapper">
-            <input id="cb-cheese" type="checkbox" checked={extraCheese} onChange={() => setExtraCheese((c) => !c)}/>
-            <label htmlFor="cb-cheese">Extra Käse</label>
+        <>
+            <div className="checkboxes-wrapper">
+                <input id="cb-cheese" type="checkbox" checked={extraCheese} onChange={() => setExtraCheese((c) => !c)}/>
+                <label htmlFor="cb-cheese">Extra Käse</label>
 
-            <input id="cb-onions" type="checkbox" checked={extraOnions} onChange={() => setExtraOnions((o) => !o)}/>
-            <label htmlFor="cb-onions">Extra Zwiebeln</label>
-
+                <input id="cb-onions" type="checkbox" checked={extraOnions} onChange={() => setExtraOnions((o) => !o)}/>
+                <label htmlFor="cb-onions">Extra Zwiebeln</label>
+            </div>
             <DebugButton log={[["Extra Cheese", extraCheese],["Extra Onions", extraOnions]]}/>
-        </div>
+        </>
+    );
+}
+
+// Aufgabe 1.2a
+function ScreenOrientation() {
+    const [orientation, setOrientation] = useState("landscape");
+
+    const handleClick = () => {
+        setOrientation((val) => val === "landscape" ? "portrait" : "landscape");
+    }
+
+    return (
+        <>
+            <div className="switch-wrapper">
+                <label htmlFor="landscape">Landscape</label>
+                <span className="switch" style={{backgroundColor: orientation === "landscape" ? "#0060df" : "transparent"}}>
+                    <input type="radio" id="landscape" name="screenOrientation" checked={orientation === "landscape"} onChange={handleClick}/>
+                    <input type="radio" id="portrait" name="screenOrientation" checked={orientation === "portrait"} onChange={handleClick}/>
+                </span>
+                <label htmlFor="portrait">Portrait</label>
+            </div>
+            <DebugButton log={[["Screen Orientation", orientation]]}/>
+        </>
     );
 }
 
@@ -57,8 +83,11 @@ function App() {
             <h3 className="app-title">Aufgabe 1</h3>
             <Flash />
 
-            <h3 className="app-title">Aufgabe 2</h3>
+            <h3 className="app-title">Aufgabe 1.1</h3>
             <CheckBoxes />
+
+            <h3 className="app-title">Aufgabe 1.2</h3>
+            <ScreenOrientation/>
         </div>
     );
 }
